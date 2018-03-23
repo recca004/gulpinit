1f328eb86fb2489fba981388e18548e71f9264d9
PA8 <PA8@ITF5DM.adds>
2018-03-23 11:43:48 +0100

    minify img

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,50 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 
 
-
+// Tâche "sass" = Sass complileur
 gulp.task('sass' , function(){
 
   return gulp.src('dev/scss/*.scss')
               .pipe(sass())
               .pipe(gulp.dest('dev/css/'))
-              .pipe(gulp.dest('prod/css/'))
+              .pipe(gulp.dest('prod/css'))
 });
-gulp.task('sassall' , function(){
 
-  return gulp.src('dev/scss/*.scss')
-              .pipe(sass())
-              .pipe(gulp.dest('dev/css'))
+// Tâche "imagemin" = image compresser 
+gulp.task('imagemin' , function(){
+
+  return gulp.src('dev/images/*.*')
+              .pipe(imagemin())
+              .pipe(rename({suffix:'.min'}))
+              .pipe(gulp.dest('dev/img'))
 });
+// Tâche "minicss" = css minify 
 gulp.task('minicss' , function(){
 
   return gulp.src('dev/css/*.css')
               .pipe(cleanCSS())
               .pipe(rename({suffix:'.min'}))
-              .pipe(gulp.dest('dev/css/'));
+              .pipe(gulp.dest('dev/css'));
+});
+// Tâche "js" = uglify 
+gulp.task('js', function() {
+  return gulp.src('dev/js/*.js')
+    .pipe(uglify())
+    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest('dev/js'));
 });
 
-gulp.task('watch' , function(){
-    gulp.watch('dev/scss/*.scss',[minicss]);
+gulp.task('watch', function(){
+   
+    
+    gulp.watch('dev/scss/', ['sass']);
+    gulp.watch('dev/css/*.css', ['minicss']);
+
+    gulp.watch('dev/images/*.*', ['imagemin']);
 
 
 
 });
 
 
-gulp.task('default', ['sass', 'sassall']);
+gulp.task('default', ['sass']);
